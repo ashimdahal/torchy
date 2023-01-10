fe5afc9620f08817f2f6e56b46712faac1841175
Ashim Dahal <codeashim@gmail.com>
2023-01-10 12:48:30 +0545

    feat: added tensordataset splitter

--- easytorch/utils/data/new_utils.py
+++ easytorch/utils/data/new_utils.py
@@ -18,3 +18,27 @@ class DeviceDL:
             
     def __len__(self):
         return len(self.dl)
+
+
+class SplitPct:
+    def __init__(self, tensor_dataset, train_pct):
+        self.train_pct = train_pct
+        self.tensor_dataset = tensor_dataset
+
+        train_num, valid_num  = self.pct_to_val()
+        self.train_ds, self.valid_ds = random_split(dataset, [train_num, valid_num])
+
+    def pct_to_val(self):
+    
+        '''Helper function to make code cleaer.
+        changes percentage split into numbers of data.
+        INPUTS:
+        train_pct: the percentage of training data 
+        valid_pct: the percentage of validation data
+        data: the dataset
+        returns: numbers of data'''
+
+        train_num = int(self.train_pct/100*len(self.tensor_dataset))
+        valid_num = int(len(self.tensor_dataset) - self.train_num)
+        return train_num , valid_num
+        
