556cc9862079a6535be82d27b0f12f36c69b7647
Ashim Dahal <codeashim@gmail.com>
2023-01-11 13:57:20 +0545

    feat: fixed bugs for python versions

--- torchy/utils/data/__init__.py
+++ torchy/utils/data/__init__.py
@@ -1,5 +1,42 @@
-from torch.utils.data import *
-
+from torch.utils.data.sampler import (
+    BatchSampler,
+    RandomSampler,
+    Sampler,
+    SequentialSampler,
+    SubsetRandomSampler,
+    WeightedRandomSampler,
+)
+from torch.utils.data.dataset import (
+    ChainDataset,
+    ConcatDataset,
+    Dataset,
+    IterableDataset,
+    Subset,
+    TensorDataset,
+    random_split,
+)
+from torch.utils.data.datapipes.datapipe import (
+    DFIterDataPipe,
+    DataChunk,
+    IterDataPipe,
+    MapDataPipe,
+)
+from torch.utils.data.dataloader import (
+    DataLoader,
+    _DatasetKind,
+    get_worker_info,
+    default_collate,
+    default_convert,
+)
+from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data.datapipes._decorator import (
+    argument_validation,
+    functional_datapipe,
+    guaranteed_datapipes_determinism,
+    non_deterministic,
+    runtime_validation,
+    runtime_validation_disabled,
+)
 from .new_utils import (
     DeviceDL,
     SplitPct
